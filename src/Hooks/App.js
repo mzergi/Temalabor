1a57024526070388bd9a51d5779db78109f8f594
mzergi <matee.zergi@gmail.com>
2020-10-04 23:41:55 +0200

    TODO: revert to main page from product page

--- src/Hooks/App.js
+++ src/Hooks/App.js
@@ -1,14 +1,19 @@
 import {useState} from 'react';
 import './App.css';
-import MainPage from '../Components/MainPage';
+import useMainPage from '../Components/MainPage';
+import useProductPage from '../Components/ProductPage';
 
 
 export default function useApp(){
   const [page, setPage] = useState(0);
 
-  let _page = MainPage({setPage: setPage});
+  
+  let main_page = useMainPage({setPage: setPage});
+
+  
+
   if(page === 0){
-    setPage(_page);
+    setPage(main_page);
   }
   return page;
 }
